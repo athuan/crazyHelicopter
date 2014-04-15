9d381226130f9f3249f448f8ad70af9f33ce3c6d
Tran Minh Luyen <tranminhluyen@gmail.com>
2014-04-15 16:44:30 +0700

    Upload all element of folder

---
+++ crazyHelicopter/crazyHelicopter/src/com/me/Helicopter/objects/Cannon.java
@@ -0,0 +1,26 @@
+package com.me.Helicopter.objects;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.me.Helicopter.game.Assets;
+
+public class Cannon extends AbstractObject {
+	
+	public Sprite cannon;
+	
+	public Cannon(){
+		cannon = Assets.instance.cannon;
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ crazyHelicopter/crazyHelicopter/src/com/me/Helicopter/objects/Tank.java
@@ -0,0 +1,27 @@
+package com.me.Helicopter.objects;
+
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.me.Helicopter.game.Assets;
+
+public class Tank extends AbstractObject {
+	
+	public Sprite tank;
+	
+	public Tank(){
+		tank = Assets.instance.tank;
+		
+	}
+
+	@Override
+	public void render() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void update() {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
