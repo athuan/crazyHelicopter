3868ea8389e4ef11d661dd8ccff89281bae88982
Tran Minh Luyen <tranminhluyen@gmail.com>
2014-05-02 15:48:46 +0700

    LuyenPro
    first commit for new versiom

--- crazyHelicopter/src/com/me/Helicopter/game/WorldController.java
+++ crazyHelicopter/src/com/me/Helicopter/game/WorldController.java
@@ -4,13 +4,8 @@ import java.util.Random;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
-import com.badlogic.gdx.Input.Keys;
-import com.badlogic.gdx.InputAdapter;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import com.badlogic.gdx.input.GestureDetector;
-import com.badlogic.gdx.input.GestureDetector.GestureListener;
-import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.utils.Array;
 import com.me.Helicopter.game.objects.Bird;
